1bb5bca7df7d57c546b37f17f148fcf071b1f7e3
sebanputhen <sebastian.puthenveedu@gmail.com>
2026-09-17 10:45:31 +0530

    Initial commit1

--- routes/familyMovement.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const familyMovementController = require('../controllers/familyMovementController');
-
-router.post('/', familyMovementController.createMovement);
-router.get('/', familyMovementController.getMovements);
-
-module.exports = router;
--- routes/parishData.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const parishController = require('../controllers/parishalloController');
-
-const router = express.Router();
-
-
-router.post('/save-data', parishController.saveParishData);
-
-module.exports = router;
